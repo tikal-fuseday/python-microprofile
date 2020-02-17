acdf07a8fed4abb3801753eabc43c73093a0eaa8
Nir Alfasi <nir@myhippo.com>
2020-02-17 18:02:17 +0200

    add unit-test for Counter + fix ctor def

--- spec/metrics/src/counter.py
+++ spec/metrics/src/counter.py
@@ -2,8 +2,8 @@ from spec.metrics.src.metric import Metric
 
 
 class Counter(Metric):
-    def __init__(self, **kwargs):
-        self.super(kwargs)
+    def __init__(self, *args, **kwargs):
+        super(Counter, self).__init__(*args, **kwargs)
         self._counter = 0
 
     def inc(self, n=1):
--- spec/metrics/src/tests/test_counter.py
+++ spec/metrics/src/tests/test_counter.py
@@ -1,14 +1,16 @@
 import unittest
 from spec.metrics.src.counter import Counter
+from spec.metrics.src.metric_units import MetricUnits
+from spec.metrics.src.metric_types import MetricTypes
 
 
 class TestCounter(unittest.TestCase):
     def test_creation(self):
-        counter = Counter()
+        counter = Counter('test_metric', MetricUnits.NONE, MetricTypes.COUNTER)
         self.assertEqual(counter.get_count(), 0)
 
     def test_inc(self):
-        counter = Counter()
+        counter = Counter('test_metric', MetricUnits.NONE, MetricTypes.COUNTER)
         counter.inc()
         self.assertEqual(counter.get_count(), 1)
         counter.inc(10)
