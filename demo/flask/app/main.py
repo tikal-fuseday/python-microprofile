4bbb8246067b34bbd666de50e159ba23c5b0e8e2
Avishay Maor <mavishay@Avishays-MBP.tikalk.com>
2020-02-17 11:34:19 +0200

    add graphql to the demo

--- demo/flask/app/main.py
+++ demo/flask/app/main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+import graphene
 
 app = Flask(__name__)
 
+class Query(graphene.ObjectType):
+  hello = graphene.String(name=graphene.String(default_value="World"))
+
+  def resolve_hello(self, info, name):
+    return 'Hello ' + name
+
+schema = graphene.Schema(query=Query)
 
 @app.route('/')
 def hello_whale():
@@ -10,4 +19,5 @@ def hello_whale():
 
 
 if __name__ == '__main__':
+    app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
     app.run(debug=True, host='0.0.0.0', port=8080)
